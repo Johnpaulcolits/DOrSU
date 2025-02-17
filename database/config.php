f9c0a687323331a3a72e73f133d9979047cd04c0
John Paul <colitajohnpaul2@gmail.com>
2025-02-17 11:52:24 +0800

    update complete

---
+++ database/config.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'dorsu';
+
+
+$conn =  new  mysqli($host,$username,$password,$database);
+
+if($conn){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+?>
+    
